42f165ee6d40b28ea2e92557fd797d9586d84af8
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-08-07 17:53:07 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
+  get '/signup', to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
