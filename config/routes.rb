88b7f28512e927f8116700fb179f61420931730c
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-08-16 18:22:24 -0700

    Setup sessions_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   get 'users/new'
 
   # get '/home', to: 'static_pages#home'
@@ -13,6 +15,10 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
 
   post '/signup',  to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
